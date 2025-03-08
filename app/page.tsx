12e13be7e7dd66e5786230dda18217e96b5284cf
Wellington Galvão <wellingtongalvao96@gmail.com>
2025-03-08 13:37:17 -0300

    feat: adição da cor primary

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Home</div>
+  return <div className="text-primary text-3xl font-bold">Home</div>
 }
--- fonts/index.ts
+++ fonts/index.ts
@@ -1,7 +1,13 @@
-import { Roboto } from "next/font/google"
+import { Roboto, Roboto_Mono } from "next/font/google"
 
 export const roboto = Roboto({
   variable: "--font-roboto",
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+})
+
+export const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"]
 })
