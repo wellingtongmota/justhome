a9acd742e3e59f67b3b2b038fce462cd49a2648f
Wellington Galvão <wellingtongalvao96@gmail.com>
2025-03-08 13:16:03 -0300

    feat: alteração da fonte principal

---
+++ fonts/index.ts
@@ -0,0 +1,7 @@
+import { Roboto } from "next/font/google"
+
+export const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+})
