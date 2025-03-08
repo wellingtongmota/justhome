22e4d2c44b982c239c9c087aa1d19806a7b5cbd8
Wellington Galvão <wellingtongalvao96@gmail.com>
2025-03-08 15:08:44 -0300

    feat: melhorias de estilo

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -10,7 +10,7 @@ export function Hero() {
         alt="Hero image city"
         className="sticky h-full w-full object-cover object-top"
       />
-      <div className="text-primary absolute top-1/2 left-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-6 p-4 text-center text-wrap">
+      <div className="text-primary absolute top-1/2 left-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-4 p-4 text-center text-wrap">
         <div className="border-primary flex place-content-center rounded-full border px-4 py-2 text-xs">
           <h2 className="uppercase">Let us guide your home</h2>
         </div>
@@ -23,7 +23,7 @@ export function Hero() {
           Search properties for sale and to rent in the UK
         </p>
 
-        <SearchInput />
+        <SearchInput className="mt-4" />
       </div>
     </section>
   )
--- components/hero/search-input.tsx
+++ components/hero/search-input.tsx
@@ -1,10 +1,26 @@
-import React from "react"
+import { cn } from "@/lib/utils"
+import { PiMagnifyingGlassLight } from "react-icons/pi"
 
-export default function SearchInput() {
+type SearchInputProps = {
+  className?: string
+}
+
+export default function SearchInput({ className }: SearchInputProps) {
   return (
-    <div className="focus-within:ring-secondary flex w-full max-w-lg gap-2 overflow-hidden rounded-full bg-white p-1 focus-within:ring-1">
-      <input type="text" className="flex-1 px-2 focus:outline-hidden" />
-      <button className="bg-secondary size-9 rounded-full text-black">S</button>
+    <div
+      className={cn([
+        "focus-within:ring-secondary flex w-full max-w-lg gap-2 overflow-hidden rounded-full bg-white p-1 text-black focus-within:ring-1",
+        className
+      ])}
+    >
+      <input
+        type="text"
+        placeholder="Enter Name, Keywords..."
+        className="flex-1 px-4 focus:outline-hidden"
+      />
+      <button className="bg-secondary hover:bg-secondary/90 grid size-9 cursor-pointer place-content-center rounded-full">
+        <PiMagnifyingGlassLight />
+      </button>
     </div>
   )
 }
